995505fcc4b113bf7e41dbfdc7f1b22cc9e902f2
wuyang910217 <wuyang910217@gmail.com>
2018-01-01 20:40:23 +0800

    optimize

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,68 @@
 import * as THREE from 'three'
 
-//创建场景.
-let scene = new THREE.Scene()
+class Demo {
+  private scene: THREE.Scene
+  private camera: THREE.PerspectiveCamera
+  private renderer: THREE.WebGLRenderer
+  private material: THREE.MeshNormalMaterial
+  private geometry: THREE.BoxGeometry
+  private cube: THREE.Mesh
 
-// 相机
-let camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000,
-)
-//渲染器
-let renderer = new THREE.WebGLRenderer()
-//设置画布大小
-renderer.setSize(window.innerWidth, window.innerHeight)
-//加入到body
-document.body.appendChild(renderer.domElement)
+  private width: number = window.innerWidth
+  private height: number = window.innerHeight
 
-//第二步,创建几何体.
+  constructor() {
+    this.init()
+  }
 
-let geometry = new THREE.BoxGeometry(1, 1, 1)
-let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-let cube = new THREE.Mesh(geometry, material)
-//加入到场景
-scene.add(cube)
+  init(): void {
+    //创建场景.
+    this.scene = new THREE.Scene()
 
-//设置相机位置
-camera.position.z = 5
+    // 相机
+    this.camera = new THREE.PerspectiveCamera(
+      75,
+      this.width / this.height,
+      0.1,
+      1000,
+    )
 
-//渲染循环
-function animate() {
-  requestAnimationFrame(animate)
-  renderer.render(scene, camera)
+    //渲染器
+    this.renderer = new THREE.WebGLRenderer()
+    //设置画布大小
+    this.renderer.setSize(this.width, this.height)
+    //加入到body
+    document.body.appendChild(this.renderer.domElement)
+
+    window.addEventListener('resize', () => {
+      this.width = window.innerWidth
+      this.height = window.innerHeight
+      this.renderer.setSize(this.width, this.height)
+      this.camera.aspect = this.width / this.height
+      this.camera.updateProjectionMatrix()
+    })
+
+    // let control = new THREE.OrbitControls(this.camera, this.renderer.domElement)
+
+    //第二步,创建几何体.
+    this.geometry = new THREE.BoxGeometry(1, 1, 1)
+    this.material = new THREE.MeshNormalMaterial()
+    this.cube = new THREE.Mesh(this.geometry, this.material)
+    //加入到场景
+    this.scene.add(this.cube)
+
+    //设置相机位置
+    this.camera.position.z = 5
+  }
+
+  //渲染循环
+  public animate = () => {
+    requestAnimationFrame(this.animate)
+    this.cube.rotation.x += 0.1
+    this.cube.rotation.y += 0.1
+    this.renderer.render(this.scene, this.camera)
+  }
 }
-animate()
+
+const demo = new Demo()
+demo.animate()
